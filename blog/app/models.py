22ba9a9c9009bc8acb3fcc6735a5928e0fb89d02
RaulDev <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>
2023-05-01 01:56:31 -0400

    agregando un modelo y migraciones para la bd

--- blog/app/models.py
+++ blog/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Publicacion(models.Model):
+    titulo  = models.CharField(max_length=20)
+    descripcion = models.TextField(max_length=250)
+    email   = models.EmailField(blank=True, null=True)
