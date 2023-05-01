11963e232db220dfe67b1cb840f9e6c5a777c85c
RaulDev <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>
2023-05-01 01:36:51 -0400

    agregando el paso de variables a traves de la vista

--- blog/app/views.py
+++ blog/app/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'app/home.html')
+    nombre= 'Admin'
+    return render(request,'app/home.html',{'nombre':nombre})
